ae3bd4e752a3e73102d2a788162c0ceef0c61158
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-02-02 09:57:29 +0100

    Update Main.java

--- src/ej1_reloj/Main.java
+++ src/ej1_reloj/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Reloj reloj1 = new Reloj("Casio", "X-300", 100, 10, true);
 		Reloj reloj2 = new Reloj("Rolex", "J-5000", 400, 5, false);
 
-		System.out.println(reloj1.getPrecio() + reloj2.getPrecio());
+		System.out.println("Suma de los precios: " + (reloj1.getPrecio() + reloj2.getPrecio()));
 
 	}
 
